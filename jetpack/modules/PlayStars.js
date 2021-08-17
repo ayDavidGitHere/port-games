77a6e51a26ef337d9cfdc60383d2f1058e14f729
aydavid <adewoleayoade057@gmail.com>
2021-08-17 18:50:04 +0100

    overwrite jetpack

--- jetpack/modules/PlayStars.js
+++ jetpack/modules/PlayStars.js
@@ -24,52 +24,83 @@ try{
     
     
     let sunColor = "goldenrod";  "#febb22"
-    let sunArc = new CDraw.arc(0, 0, CW/10, 0, 6.3, "_"+sunColor);
+    let sunArc = new CDraw.arc(0, 0, CW/15*40, 0, 6.3, "_"+sunColor);
     sunArc.alpha = 0.9;
-    sunArc.x = 0+sunArc.radius;    sunArc.y = 0+sunArc.radius; 
-    sunArc.initY = sunArc.y
+    sunArc.x = 0+sunArc.radius*-0.7;    
+    sunArc.y = 0+sunArc.radius*-0.7; 
+    sunArc.initRadius = sunArc.radius
     sunArc.GCParams.shadow = [1, 1, sunColor, sunArc.radius*2];
-    scene.add(sunArc)
+    
+    function Planet(dx, dy, radius, planetColor){
+    let planetColorDim ="rgba(150, 120, 0, 0.4)"
+    let planetArc = new CDraw.arc(0, 0, radius, 0, 6.3, "_"+planetColor);
+    planetArc.x = sunArc.x+sunArc.radius+dx;    
+    planetArc.y = sunArc.y+sunArc.radius+dy;
+    planetArc.GCParams.shadow =[0.1, 0.1, planetColorDim, 0];
+    return planetArc
+    }
+    let earthArc = Planet(10, 10, sunArc.radius/103, "#3300aa");
+    let mercuryArc = Planet(0, 0, sunArc.radius/103, "red");
+    earthArc.rotation.rad += 6.35/16;
+    
     
     
     let tStars = []
-    for(let i=0; 15>i; i++){
+    for(let i=0; 120>i; i++){
     let tStarColor = MHelp.randOpt("white", "crimson", "goldenrod")
     let tStar = new CDraw.arc(CW/2, CH/2, CW/5, 0, 6.3, "_"+tStarColor);
 
     tStar.x = Math.random()*CW*2; tStar.y = Math.random()*CH;
-    tStar.radius = 0.2+Math.random()*0.9;
-    tStar.GCParams.shadow = [0.1, 0.1, tStarColor, tStar.radius*2];
+    tStar.radius = 0.1+Math.random()*0.5;
+    tStar.GCParams.shadow = [0.1, 0.1, "transparent", 0];//tStar.radius*2];
     tStar.alpha = 1;
+    tStar.rotationSpeed = 0.0005*Math.random()*MHelp.randOpt(1,-1);
     tStars.push(tStar)
     scene.add(tStar)
     }
+    scene.add(sunArc)
+    scene.add(earthArc)
+    scene.add(mercuryArc)
+    
+    
+    
+    
     
     let sBRect = new CDraw.rect(CW-90, 90, CH-30, 30, "1_green");
-    let sBRectText = new CDraw.text("10pt Arial", "10", sBRect.x, sBRect.y-0, "_white");
+    let sBRectText = 
+    new CDraw.text("10pt Arial", "10", sBRect.x, sBRect.y-0, "_white");
     scene.add(sBRectText)
     scene.add(sBRect)
     
     
 
-    
+    console.log(earthArc.x, earthArc.y)
     let animate = function(){
         sunArc.GCParams.shadow[3] = sunArc.radius*2;
         
         
         tStars.map((tStar)=>{
-            tStar.x--;
-            if(tStar.x<0){    
+            //tStar.x--;
+            if(tStar.x<0 && false){    
                 tStar.x = CW+Math.random()*CW; tStar.y = Math.random()*CH;
                 tStar.radius = 0.2+Math.random()*0.9; 
                 tStar.color = 
                 MHelp.randOpt("white", "crimson", "goldenrod")
             }
+            tStar.rotation.rad += tStar.rotationSpeed;
+            tStar.rotation.about = sunArc.center;
         })//EO map
         
-        if(touched.active)sunArc.y-= 0.02;
-        else if(sunArc.initY > sunArc.y)sunArc.y+=0.3
         
+        earthArc.rotation.rad += 0.003;
+        earthArc.rotation.about = sunArc.center;
+        earthArc.radius = sunArc.radius/103;
+        mercuryArc.rotation.rad += 0.003;
+        mercuryArc.rotation.about = sunArc.center;
+        
+        
+        if(touched.active)sunArc.radius/= 1.02;
+        else if(sunArc.initRadius > sunArc.radius)sunArc.radius*=1.02;
         
         
         requestAnimationFrame(animate)
