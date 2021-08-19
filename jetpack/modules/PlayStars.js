39872aa5a96ebc95f2c9ff41a6f7320e5952c8a2
aydavid <adewoleayoade057@gmail.com>
2021-08-19 21:35:48 +0100

    overwrite jetpack

--- jetpack/modules/PlayStars.js
+++ jetpack/modules/PlayStars.js
@@ -32,16 +32,15 @@ try{
     sunArc.GCParams.shadow = [1, 1, sunColor, sunArc.radius*2];
     
     function Planet(dx, dy, radius, planetColor){
-    let planetColorDim ="rgba(150, 120, 0, 0.4)"
     let planetArc = new CDraw.arc(0, 0, radius, 0, 6.3, "_"+planetColor);
+    planetArc.GCParams.shadow = [0.1, 0.1, sunColor, 0];//planetArc.radius*2];
     planetArc.x = sunArc.x+sunArc.radius+dx;    
     planetArc.y = sunArc.y+sunArc.radius+dy;
-    planetArc.GCParams.shadow =[0.1, 0.1, planetColorDim, 0];
     return planetArc
     }
     let earthArc = Planet(10, 10, sunArc.radius/103, "#3300aa");
     let mercuryArc = Planet(0, 0, sunArc.radius/103, "red");
-    earthArc.rotation.rad += 6.35/16;
+    earthArc.rotation.rad += 6.28/16;
     
     
     
@@ -49,7 +48,6 @@ try{
     for(let i=0; 60>i; i++){
     let tStarColor = MHelp.randOpt("white", "crimson", "goldenrod")
     let tStar = new CDraw.arc(CW/2, CH/2, CW/5, 0, 6.3, "_"+tStarColor);
-
     tStar.x = Math.random()*CW*2; tStar.y = Math.random()*CH;
     tStar.radius = 0.1+Math.random()*0.5;
     tStar.GCParams.shadow = [0.1, 0.1, "transparent", 0];//tStar.radius*2];
@@ -62,18 +60,61 @@ try{
     scene.add(earthArc)
     scene.add(mercuryArc)
     
+    let satBound = {};
+    let satColor = "red";
+    let satWing1 = new CDraw.rect(0, 20, 0, 20, "_"+satColor);
+    let satWing2 = new CDraw.rect(0, 20, 0, 20, "_"+satColor);
+    let satSpine = new CDraw.rect(0, 20, 0, 20, "_"+satColor);
+    let satArc = new CDraw.arc(CW/5, CH/3, 8, 0, 6.3, "_"+satColor);
+    scene.add(satSpine);
+    scene.add(satWing1); scene.add(satWing2);
+    scene.add(satArc)
     
     
     
+    let gStars = [];
+    for(let i=0; 6>i; i++){
+    let gStarColor = MHelp.randOpt("white", "crimson", "goldenrod")
+    let gStar = new CDraw.rect(0, 20, 0, 20, "_"+gStarColor);
+    gStar.x = Math.random()*CW; gStar.y = Math.random()*CH/2;
+    gStar.lengthX = 10+Math.random()*10; gStar.breadthY = 5;
+    gStar.GCParams.shadow = [0.1, 0.1, gStarColor, gStar.breadthY];
+    gStar.speedX = 2+Math.random()*2.5
+    gStars.push(gStar)
+    scene.add(gStar)
+    }
     
-    let sBRect = new CDraw.rect(CW-90, 90, CH-30, 30, "1_green");
+    
+    
+    let sBRect =new CDraw.rect(CW-90, 90, CH-30, 30,"_hsla(30,100%,50%,0.5)");
     let sBRectText = 
     new CDraw.text("10pt Arial", "10", sBRect.x, sBRect.y-0, "_white");
-    scene.add(sBRectText)
-    scene.add(sBRect)
+    scene.add(sBRectText);
+    scene.add(sBRect);
     
     
-
+    
+    let dots = []; let dotColor = "red"
+    let limit = CH;
+    for(hyp=0; hyp<limit; hyp=hyp+10){
+    let dot = new CDraw.rect(0,0,0,0, "_"+dotColor);
+    dot.hyp=hyp;
+    dots.push(dot)
+    scene.add(dot);
+    }
+    function direct(sX, sY, rad, use){
+        dots.map((dot)=>{
+        dot.x = sX+Math.sin(rad)*dot.hyp;
+        dot.y = sY+Math.cos(rad)*dot.hyp;
+        dot.rotation.rad = rad*-2; dot.rotation.about=dot.center
+        if(!use)dot.lengthX=dot.breadthY=0;
+        else{dot.lengthX=2; dot.breadthY=2}
+        });
+    }
+    
+    
+    
+    
     console.log(earthArc.x, earthArc.y)
     let animate = function(){
         sunArc.GCParams.shadow[3] = sunArc.radius*2;
@@ -87,9 +128,10 @@ try{
                 tStar.color = 
                 MHelp.randOpt("white", "crimson", "goldenrod")
             }
-            //tStar.rotation.rad += tStar.rotationSpeed;
-            //tStar.rotation.about = sunArc.center;
+            
         })//EO map
+        
+        
         
         
         earthArc.rotation.rad += 0.003;
@@ -98,31 +140,85 @@ try{
         mercuryArc.rotation.rad += 0.003;
         mercuryArc.rotation.about = sunArc.center;
         
-        earthArc.realX = earthArc.rotation.about.x+
-        Math.cos(earthArc.rotation.rad)*
-        (earthArc.x-earthArc.rotation.about.x);
-        earthArc.realY = 
-        earthArc.rotation.about.y+
-        Math.sin(earthArc.rotation.rad)*
-        (earthArc.y-earthArc.rotation.about.y);
-        console.log(earthArc.realX, earthArc.realY)
-        
         if(touched.active && false)sunArc.radius/= 1.02;
         else if(sunArc.initRadius > sunArc.radius)sunArc.radius*=1.02;
+        
+        /*
+        if(touched.active && satArc.y>0)
+        {satArc.y--; satArc.color="blue";}
+        else if(satArc.y<CH){ satArc.y*=1.005; satArc.color="red";}
+        */
+        
+        
+        satSpine.lengthX = satArc.radius*5;
+        satSpine.x = satArc.x-satSpine.lengthX/2;
+        satSpine.y = satArc.y- satSpine.breadthY/2;
+        satSpine.breadthY = satArc.radius/3;
+        
+        satWing1.lengthX = satWing1.breadthY =
+        satWing2.lengthX = satWing2.breadthY = satArc.radius*2;
+        satWing1.x = satSpine.x- satWing1.lengthX/2;
+        satWing2.x = satSpine.x+satSpine.lengthX- satWing2.lengthX/2;
+        satWing1.y = satWing2.y = satArc.y- satWing1.breadthY/2;
+        satBound = 
+        {xMin:satWing1.x,yMin:satWing1.y,
+        xMax:satWing2.x+satWing2.lengthX, yMax: satWing2.y+satWing2.breadthY}
+        satWing2.rotation.rad +=0.01; satWing2.rotation.about = satArc.center
+        satWing1.rotation.rad +=0.01; satWing1.rotation.about = satArc.center;
+        satSpine.rotation.rad +=0.01; satSpine.rotation.about = satArc.center
+        
+        
+        
+        if(touched.active&&touched.forDir){
+            let rad = 1.6-Math.atan((touched.y-satArc.y)/satArc.x);
+            console.log(rad)
+            direct(satArc.x, satArc.y, rad, true);
+        }
+        else direct(satArc.x, satArc.y, 1.57, false);
+        
+        
+        
+        
+        
+        gStars.map((gStar)=>{
+            gStar.x-=gStar.speedX;
+            if(gStar.x+gStar.lengthX<0 ){    
+                gStar.x = CW+Math.random()*CW; gStar.y = Math.random()*CH/2;
+                gStar.lengthX = 10+Math.random()*10; gStar.breadthY = 5;
+                gStar.GCParams.shadow=[0.1, 0.1, gStar.color, gStar.breadthY];
+                gStar.speedX = 1+Math.random()*1.5
+            }
+            if(gStar.x>=satBound.xMin&&gStar.x<=satBound.xMax&&gStar.y>=satBound.yMin&&gStar.y<=gStar.yMax){
+                console.log(hit);
+                gStar.x=-CW;
+            }
+        })//EO map
+        
+        
+        
+        
+        
+        
         
         
         requestAnimationFrame(animate)
     }
     animate();
+    console.log(satBound);
+    console.log(gStars[0])
     
     
-    
-    a.ontouchstart = function(ev){
-        touched.active = true;
+    a.ontouchstart = function(e){
+        let tX = e.touches[0].pageX; let tY=e.touches[0].pageY;
+        let forDir = (tX<=CW/2?true:false);
+        touched={active:true,x:tX,y:tY, forDir:forDir};
     }
-    a.ontouchend = function(ev){
-        touched.active = false;
+    a.ontouchmove = function(e){touched.active = true;
+        let tX = e.touches[0].pageX; let tY=e.touches[0].pageY;
+        let forDir = (tX<=CW/2?true:false);
+        touched={active:true,x:tX,y:tY, forDir:forDir};
     }
+    a.ontouchend = function(ev){touched.active = false;}
     
     
     
